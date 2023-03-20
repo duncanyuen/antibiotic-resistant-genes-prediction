5f3400d7b9583c21e9bff8b3be609d9a449e1f1a
Duncan Yuen <duncanyuen@gmail.com>
2023-03-20 15:17:14 +0800

    progress

--- AIST4010-A2.py
+++ AIST4010-A2.py
@@ -1,6 +1,11 @@
 from Bio import SeqIO
 
 #load dataset
+train_data = SeqIO.parse("./data/train.fasta", "fasta")
+val_data = SeqIO.parse("./data/val.fasta", "fasta")
+print(type(train_data))
+train_data = list(train_data)
+print(type(train_data[0]))
 
 #load model
 
