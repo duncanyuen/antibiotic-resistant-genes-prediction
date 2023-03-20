cc1d6e96bb4d4962781411a5229a9728bb1b5531
Duncan Yuen <duncanyuen@gmail.com>
2023-03-20 18:09:41 +0800

    progress

--- AIST4010-A2.py
+++ AIST4010-A2.py
@@ -1,11 +1,30 @@
 from Bio import SeqIO
 
+# define dataset class
+class ProteinDataset(Dataset):
+    def __init__(self, txt, labels):
+        self.labels = labels
+        self.text = text
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        label = self.labels[idx]
+        text = self.text[idx]
+        sample = {"Text": text, "Class": label}
+        return sample
+
+
+
 #load dataset
 train_data = SeqIO.parse("./data/train.fasta", "fasta")
 val_data = SeqIO.parse("./data/val.fasta", "fasta")
 print(type(train_data))
 train_data = list(train_data)
 print(type(train_data[0]))
+
+dataloader = 
 
 #load model
 
