061e811d5f33b341ddfd3f5346a2e8fff3896578
Duncan Yuen <duncanyuen@gmail.com>
2023-03-22 01:16:07 +0800

    progress

--- AIST4010-A2.py
+++ AIST4010-A2.py
@@ -185,35 +185,35 @@ torch.save(val_dataset, "./valdata.pt")
 
 #load model
 
-
-training_args = TrainingArguments(
-    output_dir='./results',          # output directory
-    num_train_epochs=1,              # total number of training epochs
-    per_device_train_batch_size=1,   # batch size per device during training
-    per_device_eval_batch_size=10,   # batch size for evaluation
-    warmup_steps=1000,               # number of warmup steps for learning rate scheduler
-    weight_decay=0.01,               # strength of weight decay
-    logging_dir='./logs',            # directory for storing logs
-    logging_steps=200,               # How often to print logs
-    do_train=True,                   # Perform training
-    do_eval=True,                    # Perform evaluation
-    evaluation_strategy="epoch",     # evalute after eachh epoch
-    gradient_accumulation_steps=64,  # total number of steps before back propagation
-    fp16=True,                       # Use mixed precision
-    fp16_opt_level="02",             # mixed precision mode
-    run_name="ProBert-BFD-ARG",       # experiment name
-    seed=3                           # Seed for experiment reproducibility 3x3
-)
-
-trainer = Trainer(
-    model_init=model_init,                # the instantiated 🤗 Transformers model to be trained
-    args=training_args,                   # training arguments, defined above
-    train_dataset=train_dataset,          # training dataset
-    eval_dataset=val_dataset,             # evaluation dataset
-    compute_metrics = compute_metrics,    # evaluation metrics
-)
-
-trainer.train()
-
-trainer.save_model('models/')
+#
+# training_args = TrainingArguments(
+#     output_dir='./results',          # output directory
+#     num_train_epochs=1,              # total number of training epochs
+#     per_device_train_batch_size=1,   # batch size per device during training
+#     per_device_eval_batch_size=10,   # batch size for evaluation
+#     warmup_steps=1000,               # number of warmup steps for learning rate scheduler
+#     weight_decay=0.01,               # strength of weight decay
+#     logging_dir='./logs',            # directory for storing logs
+#     logging_steps=200,               # How often to print logs
+#     do_train=True,                   # Perform training
+#     do_eval=True,                    # Perform evaluation
+#     evaluation_strategy="epoch",     # evalute after eachh epoch
+#     gradient_accumulation_steps=64,  # total number of steps before back propagation
+#     fp16=True,                       # Use mixed precision
+#     fp16_opt_level="02",             # mixed precision mode
+#     run_name="ProBert-BFD-ARG",       # experiment name
+#     seed=3                           # Seed for experiment reproducibility 3x3
+# )
+#
+# trainer = Trainer(
+#     model_init=model_init,                # the instantiated 🤗 Transformers model to be trained
+#     args=training_args,                   # training arguments, defined above
+#     train_dataset=train_dataset,          # training dataset
+#     eval_dataset=val_dataset,             # evaluation dataset
+#     compute_metrics = compute_metrics,    # evaluation metrics
+# )
+#
+# trainer.train()
+#
+# trainer.save_model('models/')
 #fine-tune
